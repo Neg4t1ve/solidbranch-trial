82b2cc8e6cfd491b3905d06b1423cb4e946096e6
Andrei Osadchii <nc1xo099@gmail.com>
2022-10-11 18:26:50 +0400

    service unit tests

--- src/app/services/transactions.service.spec.ts
+++ src/app/services/transactions.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-
+import mock from '../../files/transactions.json';
 import { TransactionsService } from './transactions.service';
 
 describe('TransactionsServiceService', () => {
@@ -12,5 +12,18 @@ describe('TransactionsServiceService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should give total count of transactions', () => {
+    expect(service.getTotal()).toBe(mock.total);
+  });
+
+  it('should return empty array if wrong type', () => {
+    expect(service.getFilteredList('wrong')).toEqual([]);
+  });
+
+  it('should return filtered array', () => {
+    const filteredMock = mock.data.filter((i) => i.type === 'loan');
+    expect(service.getFilteredList('loan')).toEqual(filteredMock);
   });
 });
